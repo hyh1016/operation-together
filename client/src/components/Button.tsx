391ec9a10bb5d01d7b78c218facfc348fd565c89
Yihyun <hihyun16@gmail.com>
2021-01-07 19:31:05 +0900

    Feat: create Button component - 재사용 가능한 버튼 컴포넌트 생성
    Issue #4

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ButtonWrapper = styled.button`
+  width: 80%;
+  margin: 0.5rem;
+  padding: 0.3rem;
+  border: 1px solid ${(props) => props.color};
+  border-radius: ${(props) => props.theme.borderRadius};
+  background-color: ${(props) =>
+    props.color === props.theme.mainColor
+      ? props.theme.highlightColor
+      : props.theme.mainColor};
+  color: ${(props) => props.color};
+  font-size: 1.5rem;
+  outline: none;
+  cursor: pointer;
+
+  transition: all ease 0.3s 0s;
+  &:hover {
+    opacity: 0.5;
+  }
+`;
+
+interface Props {
+  value: string;
+  color: string;
+}
+
+const Button: React.FC<Props> = ({ value, color }) => {
+  return (
+    <div>
+      <ButtonWrapper color={color}>{value}</ButtonWrapper>
+    </div>
+  );
+};
+
+export default Button;
