c9b9a39a994d346e8d42c9369a249a32177c1560
Yihyun <hihyun16@gmail.com>
2021-01-07 19:33:15 +0900

    Feat: create Home component - 홈 페이지에 해당하는 Home 컴포넌트 생성
    Issue #4

---
+++ client/src/routes/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Title from '@/components/Title';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Title />
+    </div>
+  );
+};
+
+export default Home;
