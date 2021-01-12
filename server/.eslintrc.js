f087727091279ed8b176c04181b5e6010042e741
Yihyun <hihyun16@gmail.com>
2021-01-12 22:19:13 +0900

    Chore: set eslint/prettier - 구현에 필요한 최소한의 eslint/prettier 설정 - 추후 점진적으로 추가될 예정
    Issue #2

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  rules: {
+    'linebreak-style': 0,
+    'prettier/prettier': ['error'],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prefer-const': 'off',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
