e44d9fd5fedaaa9dd12045e6b12820f29a94cc88
Yihyun <hihyun16@gmail.com>
2021-01-14 21:21:43 +0900

    Feat: update config file - database 연결을 위한 config 파일 업데이트
    Issue #8

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -5,4 +5,12 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default {
   PORT: process.env.PORT,
+  DB: {
+    TYPE: process.env.DB_TYPE,
+    HOST: process.env.HOST,
+    PORT: process.env.DB_PORT,
+    NAME: process.env.DB_NAME,
+    USERNAME: process.env.DB_USERNAME,
+    PASSWORD: process.env.DB_PASSWORD,
+  },
 };
