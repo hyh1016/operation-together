f17a9a5e41e5ac5115cd826be843ce4d9d1f3fde
Yihyun <hihyun16@gmail.com>
2021-01-18 18:11:08 +0900

    Feat: create User table - 사용자 정보를 저장하는 User table 생성
    Issue #10

---
+++ server/src/entity/User.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  password!: string;
+}
