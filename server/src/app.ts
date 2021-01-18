f968eccc35a4d0dbaa22518329ec8d15813c1778
Yihyun <hihyun16@gmail.com>
2021-01-18 18:18:36 +0900

    Feat: add router to app layer - 앱단에 라우터로 진입하는 코드 추가 - 의존성 주입(DI)를 이용하기 위한 설정 추가
    Issue #11

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,45 @@
 import express from 'express';
+import { createConnection, getRepository } from 'typeorm';
+import cors from 'cors';
+import createError from 'http-errors';
 import config from './config';
+import Router from './route';
+import Container from './container';
+import UserService from '@/service/UserService';
+import { User } from './entity/User';
 
-const app = express();
+const startServer = async () => {
+  await createConnection()
+    .then(() => {
+      Container.set('UserService', new UserService(getRepository(User)));
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
-const PORT = config.PORT || 4000;
+  const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listen http://localhost:${PORT}`);
-});
+  const PORT = config.PORT || 4000;
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+
+  app.use('/', Router());
+
+  // TODO: error handling
+  app.use((_, __, next) => {
+    next(createError(404));
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server listen http://localhost:${PORT}`);
+  });
+};
+
+startServer();
