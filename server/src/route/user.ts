a0ad607860f79fcb393a64e8b679f419ecc5f5f0
Yihyun <hihyun16@gmail.com>
2021-01-22 17:23:25 +0900

    Feat: create validate user id API - 이미 사용중인 id인지 검사하는 API 추가 - Container의 타입 지정 로직 추가
    Issue #16

--- server/src/route/user.ts
+++ server/src/route/user.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Container from '@/container';
+import { WRONG_KEY } from '@/util/error';
 
 const router = Router();
 
@@ -8,17 +9,28 @@ export default (indexRouter: Router): void => {
 
   router.post('/register', async (req, res) => {
     const serviceInstance = Container.get('UserService');
+    if (!serviceInstance) throw new Error(WRONG_KEY);
     const { id, name, password } = req.body;
     const registResult = await serviceInstance.register({ id, name, password });
-    if (registResult) return res.status(200).json({ status: 'success' });
-    return res.status(400).json({ status: 'fail' });
+    if (registResult) return res.status(200).json({ token: 'token' });
+    return res.status(400).json({ message: 'This ID already exists.' });
   });
 
   router.post('/login', async (req, res) => {
     const serviceInstance = Container.get('UserService');
+    if (!serviceInstance) throw new Error(WRONG_KEY);
     const { id, password } = req.body;
     const successLogin = await serviceInstance.login({ id, password });
-    if (successLogin) return res.status(200).json({ status: 'success' });
-    return res.status(404).json({ status: 'fail' });
+    if (successLogin) return res.status(200).json({ token: 'token' });
+    return res.status(404).json();
+  });
+
+  router.get('/exist/:id', async (req, res) => {
+    const serviceInstance = Container.get('UserService');
+    if (!serviceInstance) throw new Error(WRONG_KEY);
+    const { id } = req.params;
+    const isExistUser = await serviceInstance.validateUser(id);
+    if (isExistUser) return res.status(200).json();
+    return res.status(404).json();
   });
 };
