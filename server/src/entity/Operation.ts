b0ff0e811fa5420dc3f946f75e0c8c543ae25776
Yihyun <hihyun16@gmail.com>
2021-01-22 14:19:55 +0900

    Feat: create Operation table - 작전을 저장하기 위한 Operation 테이블 생성 - 사용자와 M:N, 작전 진행도인 Chart와 1:M 관계를 가짐
    Issue #12

---
+++ server/src/entity/Operation.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import User from './User';
+import Chart from './Chart';
+
+@Entity()
+export default class Operation {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  startDate!: Date;
+
+  @Column()
+  endDate!: Date;
+
+  @Column()
+  color!: string;
+
+  @Column()
+  adminId!: string;
+
+  @ManyToMany(() => User)
+  users: User[] | undefined;
+
+  @OneToMany(() => Chart, (chart) => chart.operation)
+  charts: Chart[] | undefined;
+}
