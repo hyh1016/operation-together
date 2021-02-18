312aa11e2ef06773bd368033b1a214cda296c6bd
Yihyun <hihyun16@gmail.com>
2021-02-18 15:08:06 +0900

    Feat: add logic of validation - 회원가입 시 유효성 검증을 위한 로직 구현
    Issue #18

--- client/src/components/JoinForm.tsx
+++ client/src/components/JoinForm.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import theme from '@/style/theme';
+import { sendGetRequest } from '@/utils/request';
+import { ERROR } from '@/utils/message';
 import Button from './Button';
 
-const JoinFormWrapper = styled.div`
+const JoinFormWrapper = styled.form`
   margin-top: 1rem;
   padding-top: 1rem;
   border-top: 1px solid ${(props) => props.theme.highlightColor};
@@ -39,30 +41,64 @@ const JoinForm: React.FC = () => {
   const [inspectPassword, setInspectPassword] = useState('');
   const [message, setMessage] = useState('');
 
-  const joinEvent = () => {
-    if (!isValidForm()) return false;
-    // TODO: ajax - join
+  const isValidId = () => {
+    if (id.length < 6 || id.length > 12) return false;
+    if (/[^\w]/.test(id)) return false;
+    return true;
   };
 
-  const isValidForm = (): boolean => {
-    if (!isValidId(id)) {
-      setMessage('잘못된 형식의 아이디입니다.');
+  const isNotExistId = async () => {
+    const result = await sendGetRequest(`/users/exist/${id}`);
+    if (result.data.exist) {
       return false;
     }
-    if (!isValidNickname(nickname)) {
-      setMessage('잘못된 형식의 닉네임입니다.');
-      return false;
-    }
-    if (!isValidPassword(password)) {
-      setMessage('잘못된 형식의 비밀번호입니다.');
-      return false;
-    }
-    if (!isEqualPassword(password, inspectPassword)) {
-      setMessage('비밀번호가 틀렸습니다.');
+    return true;
+  };
+
+  const isValidNickname = () => {
+    if (nickname.length < 1 || nickname.length > 10) return false;
+    return true;
+  };
+
+  const isValidPassword = () => {
+    if (password.length < 8 || password.length > 12) return false;
+    return true;
+  };
+
+  const isEqualPassword = () => {
+    if (password !== inspectPassword) return false;
+    return true;
+  };
+
+  const checkData = (func: () => boolean, errorMessage: string) => {
+    const result = func();
+    if (!result) {
+      setMessage(errorMessage);
       return false;
     }
     setMessage('');
     return true;
+  };
+
+  const checkAsyncData = async (
+    func: () => Promise<boolean>,
+    errorMessage: string,
+  ) => {
+    const result = await func();
+    if (!result) {
+      setMessage(errorMessage);
+      return false;
+    }
+    setMessage('');
+    return true;
+  };
+
+  const isValidForm = async () => {
+    if (!checkData(isValidId, ERROR.NOT_VALID_ID)) return;
+    if (!(await checkAsyncData(isNotExistId, ERROR.IS_EXIST_ID))) return;
+    if (!checkData(isValidNickname, ERROR.NOT_VALID_NICKNAME)) return;
+    if (!checkData(isValidPassword, ERROR.NOT_VALID_PASSWORD)) return;
+    checkData(isEqualPassword, ERROR.NOT_EQUAL_PASSWORD);
   };
 
   return (
@@ -73,53 +109,51 @@ const JoinForm: React.FC = () => {
           type="text"
           value={id}
           onChange={(e) => setId(e.target.value)}
+          onBlur={async () => {
+            if (!checkData(isValidId, ERROR.NOT_VALID_ID)) return;
+            await checkAsyncData(isNotExistId, ERROR.IS_EXIST_ID);
+          }}
           placeholder="아이디 (영문, 숫자 6~12자)"
         />
         <Input
           type="text"
           value={nickname}
           onChange={(e) => setNickname(e.target.value)}
+          onBlur={() => {
+            checkData(isValidNickname, ERROR.NOT_VALID_NICKNAME);
+          }}
           placeholder="닉네임 (1~10자)"
         />
         <Input
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          onBlur={() => {
+            checkData(isValidPassword, ERROR.NOT_VALID_PASSWORD);
+          }}
           placeholder="비밀번호 (8~12자)"
         />
         <Input
           type="password"
           value={inspectPassword}
           onChange={(e) => setInspectPassword(e.target.value)}
+          onBlur={() => {
+            checkData(isEqualPassword, ERROR.NOT_EQUAL_PASSWORD);
+          }}
           placeholder="비밀번호 확인"
         />
-        <Button color={theme.mainColor} value="가입하기" onClick={joinEvent} />
+        <Button
+          color={theme.mainColor}
+          value="가입하기"
+          onClick={async (e) => {
+            e.preventDefault();
+            await isValidForm();
+          }}
+        />
         <Message>{message}</Message>
       </JoinFormWrapper>
     </>
   );
-};
-
-const isValidId = (id: string) => {
-  if (id.length < 6 || id.length > 12) return false;
-  if (/[^\w]/.test(id)) return false;
-  // TODO: 이미 가입되어있는 id인지 검사
-  return true;
-};
-
-const isValidNickname = (nickname: string) => {
-  if (nickname.length < 1 || nickname.length > 10) return false;
-  return true;
-};
-
-const isValidPassword = (password: string) => {
-  if (password.length < 8 || password.length > 12) return false;
-  return true;
-};
-
-const isEqualPassword = (password: string, inspectPassword: string) => {
-  if (password !== inspectPassword) return false;
-  return true;
 };
 
 export default JoinForm;
