097b546f0c7ab60fbe2f3cd8717e8002a5c81c48
Yihyun <hihyun16@gmail.com>
2021-02-18 15:10:25 +0900

    Feat: manage message to constant object - 오류 안내 등에 사용될 메시지를 객체로 관리
    Issue #18

---
+++ client/src/utils/message.ts
@@ -0,0 +1,9 @@
+const ERROR = {
+  NOT_VALID_ID: '잘못된 형식의 아이디입니다.',
+  IS_EXIST_ID: '이미 사용중인 아이디입니다.',
+  NOT_VALID_NICKNAME: '잘못된 형식의 닉네임입니다.',
+  NOT_VALID_PASSWORD: '잘못된 형식의 비밀번호입니다.',
+  NOT_EQUAL_PASSWORD: '비밀번호가 틀렸습니다.',
+};
+
+export { ERROR };
