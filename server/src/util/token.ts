75613a15c52ae8285838f84f1eaab4174c43bfe7
Yihyun <hihyun16@gmail.com>
2021-03-03 15:01:12 +0900

    Feat: add logic of create token, verify token - 토큰의 생성 및 검증 로직 구현
    Issue #14

---
+++ server/src/util/token.ts
@@ -0,0 +1,19 @@
+import { sign, verify } from 'jsonwebtoken';
+import config from '@/config';
+
+const createUserToken = (id: string): string => {
+  const token = sign({ id }, config.SECRET_KEY as string);
+  return token;
+};
+
+const verifyUserToken = (token: string, id: string): boolean => {
+  try {
+    const decoded: any = verify(token, config.SECRET_KEY as string);
+    if (decoded.id === id) return true;
+  } catch (e) {
+    console.error(e);
+  }
+  return false;
+};
+
+export { createUserToken, verifyUserToken };
