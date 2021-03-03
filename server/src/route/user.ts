67646fe05c14fe4418f22d83a7e8c6460c417b7d
Yihyun <hihyun16@gmail.com>
2021-03-03 15:04:44 +0900

    Feat: apply login logic to router - 라우터에 로그인 로직 적용 - 로그인 성공 시 토큰 반환
    Issue #14

--- server/src/route/user.ts
+++ server/src/route/user.ts
@@ -1,36 +1,40 @@
-import { Router } from 'express';
+import { Router, Response } from 'express';
 import Container from '@/container';
 import { WRONG_KEY } from '@/util/errorMessage';
+import passport from 'passport';
+import { createUserToken } from '@/util/token';
 
 const router = Router();
 
 export default (indexRouter: Router): void => {
   indexRouter.use('/users', router);
 
-  router.post('/register', async (req, res) => {
+  router.post('/register', async (req, res, next) => {
     const serviceInstance = Container.get('UserService');
     if (!serviceInstance) throw new Error(WRONG_KEY);
     const { id, name, password } = req.body;
     const registResult = await serviceInstance.register({ id, name, password });
-    if (registResult) return res.status(200).json({ token: 'token' });
-    return res.status(400).json({ message: 'This ID already exists.' });
+    if (!registResult)
+      return res.status(400).json({ message: 'This ID already exists.' });
+    return res.status(200).json({ success: true });
   });
 
-  router.post('/login', async (req, res) => {
-    const serviceInstance = Container.get('UserService');
-    if (!serviceInstance) throw new Error(WRONG_KEY);
-    const { id, password } = req.body;
-    const successLogin = await serviceInstance.login({ id, password });
-    if (successLogin) return res.status(200).json({ token: 'token' });
-    return res.status(404).json();
-  });
+  router.post(
+    '/login',
+    passport.authenticate('local', { failureRedirect: '/login' }),
+    (req: any, res: Response) => {
+      if (!req.user) return res.status(400).json({ message: 'fail login' });
+      const token = createUserToken(req.user.id);
+      return res.status(200).json({ token });
+    },
+  );
 
   router.get('/exist/:id', async (req, res) => {
     const serviceInstance = Container.get('UserService');
     if (!serviceInstance) throw new Error(WRONG_KEY);
     const { id } = req.params;
     const isExistUser = await serviceInstance.validateUser(id);
-    if (isExistUser) return res.status(200).json();
-    return res.status(404).json();
+    if (isExistUser) return res.status(200).json({ exist: true });
+    return res.status(204).json({ exist: false });
   });
 };
