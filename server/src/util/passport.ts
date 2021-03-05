01466709e85f24722c97cc3c894661ac51e4da7c
Yihyun <hihyun16@gmail.com>
2021-03-05 14:53:23 +0900

    Feat: modify jwt strategy - jwt 인증 전략 수정
    Issue #14

--- server/src/util/passport.ts
+++ server/src/util/passport.ts
@@ -1,30 +1,17 @@
 import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import Container from '@/container';
-import { WRONG_KEY, WRONG_DATA } from './errorMessage';
-import User from '@/entity/User';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import config from '@/config';
 
-const passportConfig = { usernameField: 'id', passwordField: 'password' };
-
-const passportVerify = async (id: string, password: string, done: any) => {
-  try {
-    const serviceInstance = Container.get('UserService');
-    if (!serviceInstance) throw new Error(WRONG_KEY);
-    const user = await serviceInstance.login({ id, password });
-    if (!user) done(null, false, WRONG_DATA);
-    done(null, user);
-  } catch (e) {
-    console.error(e);
-    done(e);
-  }
+const jwtConfig = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: config.SECRET_KEY,
 };
 
-export default (): void => {
-  passport.serializeUser((user, done) => {
-    return done(null, user);
-  });
-  passport.deserializeUser((user: User, done) => {
-    return done(null, user);
-  });
-  passport.use('local', new LocalStrategy(passportConfig, passportVerify));
+export default () => {
+  passport.use(
+    new JwtStrategy(jwtConfig, (payload, done) => {
+      console.log(payload);
+      return done(null, payload);
+    }),
+  );
 };
