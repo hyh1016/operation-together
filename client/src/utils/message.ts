d1df980b1fa275ce0b0c3a6d084a18ef8bc1686b
Yihyun <hihyun16@gmail.com>
2021-03-16 14:48:11 +0900

    Feat: add error message - 에러 안내에 사용되는 메시지 추가
    Issue #18

--- client/src/utils/message.ts
+++ client/src/utils/message.ts
@@ -4,6 +4,9 @@ const ERROR = {
   NOT_VALID_NICKNAME: '잘못된 형식의 닉네임입니다.',
   NOT_VALID_PASSWORD: '잘못된 형식의 비밀번호입니다.',
   NOT_EQUAL_PASSWORD: '비밀번호가 틀렸습니다.',
+  NOT_VALID_USER: '아이디 또는 비밀번호가 일치하지 않습니다.',
+  JOIN_FAILED: '회원가입에 실패하였습니다.',
+  LOGIN_FAILED: '로그인에 실패하였습니다.',
 };
 
 export { ERROR };
