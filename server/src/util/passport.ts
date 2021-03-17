16e9487c6dd9a4c62b88e2bfce2d60bec5ae5082
Yihyun <hihyun16@gmail.com>
2021-03-17 14:55:15 +0900

    Feat: add getUser to passport-jwt strategy - 토큰 유효성을 검증하는 passport jwt 전략에서 id 값을 받아 user를 찾은 뒤 돌려주도록 함 - req.user의 의미를 반영하기 위함
    Issue #23

--- server/src/util/passport.ts
+++ server/src/util/passport.ts
@@ -1,17 +1,20 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import config from '@/config';
+import Container from '@/container';
 
 const jwtConfig = {
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.SECRET_KEY,
 };
 
-export default () => {
+export default (): void => {
   passport.use(
-    new JwtStrategy(jwtConfig, (payload, done) => {
-      console.log(payload);
-      return done(null, payload);
+    new JwtStrategy(jwtConfig, async (payload, done) => {
+      const serviceInstance = Container.get('UserService');
+      const user = await serviceInstance.getUser(payload.id);
+      if (!user) return done(null, false);
+      return done(null, user);
     }),
   );
 };
