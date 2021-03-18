a2c0d612b88ef8731730db89617977c574747575
Yihyun <hihyun16@gmail.com>
2021-03-18 21:31:58 +0900

    Feat: add join, leave operation API - 작전에 참가, 퇴장하기 위한 API 추가
    Issue #25

--- server/src/route/operation.ts
+++ server/src/route/operation.ts
@@ -36,11 +36,42 @@ export default (indexRouter: Router): void => {
   });
 
   router.get('/:id', async (req, res) => {
-    const { id } = req.params;
+    const { id: operationId } = req.params;
+    const { id: userId } = req.user as User;
     const serviceInstance = Container.getOperationService();
-    const operation = await serviceInstance.getOperationById(Number(id));
+    const isValidUser = await serviceInstance.isValidUser(
+      userId,
+      Number(operationId),
+    );
+    if (!isValidUser) return res.status(401).json();
+    const operation = await serviceInstance.getOperationById(
+      Number(operationId),
+    );
     if (!operation) return res.status(204).json();
     return res.status(200).json({ operation });
+  });
+
+  router.put('/join', async (req, res) => {
+    const { id, password } = req.body;
+    const serviceInstance = Container.getOperationService();
+    const isJoin = await serviceInstance.joinOperation(req.user as User, {
+      id: Number(id),
+      password,
+    });
+    if (!isJoin) return res.status(400).json({ message: 'join failed' });
+    return res.status(200).json();
+  });
+
+  router.put('/leave/:id', async (req, res) => {
+    const { id: operationId } = req.params;
+    const { id: userId } = req.user as User;
+    const serviceInstance = Container.getOperationService();
+    const isLeave = await serviceInstance.leaveOperation(
+      userId,
+      Number(operationId),
+    );
+    if (!isLeave) return res.status(400).json({ message: 'leave failed' });
+    return res.status(200).json();
   });
 
   router.put('/:id', async (req, res) => {
