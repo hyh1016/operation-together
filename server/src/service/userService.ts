ecb99fb842d26e52d497da760299746fcfa0d133
Yihyun <hihyun16@gmail.com>
2021-03-19 14:49:24 +0900

    Refactor: export interface, add try-catch to userService - user 서비스 객체에서 인터페이스를 제공 - user 서비스 객체의 예외 처리
    Issue #26

--- server/src/service/userService.ts
+++ server/src/service/userService.ts
@@ -1,15 +1,15 @@
 import { Repository } from 'typeorm';
-import { compare, hash } from 'bcrypt';
+import { compare } from 'bcrypt';
 import User from '@/entity/User';
 import CreateHashPassword from '@/util/encryption';
 
-interface RegisterForm {
+export interface RegisterForm {
   id: string;
   nickname: string;
   password: string;
 }
 
-interface LoginForm {
+export interface LoginForm {
   id: string;
   password: string;
 }
@@ -22,26 +22,41 @@ export default class UserService {
     const isExistUser = await this.getUser(info.id);
     if (isExistUser) return false;
     const hashPassword = await CreateHashPassword(info.password);
-    const newUser = this.userRepository.create({
-      ...info,
-      password: hashPassword,
-    });
-    await this.userRepository.save(newUser);
+    try {
+      const newUser = this.userRepository.create({
+        ...info,
+        password: hashPassword,
+      });
+      await this.userRepository.save(newUser);
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
     return true;
   }
 
   async login(info: LoginForm): Promise<User | undefined> {
     if (Object.values(info).filter((v) => !v).length > 0) return undefined;
-    const user = await this.userRepository.findOne({ id: info.id });
-    if (!user) return undefined;
-    const compareResult = await compare(info.password, user.password);
-    if (!compareResult) return undefined;
-    return user;
+    try {
+      const user = await this.userRepository.findOne({ id: info.id });
+      if (!user) return undefined;
+      const compareResult = await compare(info.password, user.password);
+      if (!compareResult) return undefined;
+      return user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 
   async getUser(id: string): Promise<User | undefined> {
     if (!id) return undefined;
-    const user = await this.userRepository.findOne({ id });
-    return user;
+    try {
+      const user = await this.userRepository.findOne({ id });
+      return user;
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
   }
 }
