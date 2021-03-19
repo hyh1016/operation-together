fedebd7be25e8fb71344db6c5ca81e7c710358db
Yihyun <hihyun16@gmail.com>
2021-03-19 14:50:21 +0900

    Refactor: modify user router - service 객체가 제공하는 type interface를 이용
    Issue #26

--- server/src/route/user.ts
+++ server/src/route/user.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import Container from '@/container';
+import { RegisterForm, LoginForm } from '@/service/userService';
 import { createUserToken } from '@/util/token';
 
 const router = Router();
@@ -9,12 +10,8 @@ export default (indexRouter: Router): void => {
 
   router.post('/register', async (req, res) => {
     const serviceInstance = Container.getUserService();
-    const { id, nickname, password } = req.body;
-    const registResult = await serviceInstance.register({
-      id,
-      nickname,
-      password,
-    });
+    const userDTO: RegisterForm = req.body;
+    const registResult = await serviceInstance.register(userDTO);
     if (!registResult)
       return res.status(400).json({ message: 'registration failed' });
     return res.status(200).json({ success: true });
@@ -22,8 +19,8 @@ export default (indexRouter: Router): void => {
 
   router.post('/login', async (req, res) => {
     const serviceInstance = Container.getUserService();
-    const { id, password } = req.body;
-    const user = await serviceInstance.login({ id, password });
+    const userDTO: LoginForm = req.body;
+    const user = await serviceInstance.login(userDTO);
     if (!user) return res.status(400).json({ message: 'login failed' });
     const token = createUserToken(user.id);
     return res.status(200).json({ token });
@@ -33,7 +30,7 @@ export default (indexRouter: Router): void => {
     const serviceInstance = Container.getUserService();
     const { id } = req.params;
     const user = await serviceInstance.getUser(id);
-    if (user) return res.status(200).json({ exist: true });
-    return res.status(200).json({ exist: false });
+    if (!user) return res.status(200).json({ exist: false });
+    return res.status(200).json({ exist: true });
   });
 };
