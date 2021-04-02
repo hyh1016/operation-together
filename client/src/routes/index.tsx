c6bcb88872da8f00e2c4115a40f251195e6ba4dd
Yihyun <hihyun16@gmail.com>
2021-04-02 15:14:40 +0900

    Feat: add main router to index router - 라우팅 과정에 메인 라우터를 추가
    Issue #19

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './Home';
+import Main from './Main';
 
 const Routes: React.FC = () => {
   return (
     <>
       <Route exact path="/" component={Home} />
+      <Route exact path="/main" component={Main} />
     </>
   );
 };
