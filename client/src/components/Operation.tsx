b4114c70b5a1b9d3650ab351492005027443de4c
Yihyun <hihyun16@gmail.com>
2021-04-02 15:08:21 +0900

    Feat: create Operation Component - 작전 정보를 담는 Operation 컴포넌트 생성 - Button 컴포넌트의 재사용
    Issue #19

---
+++ client/src/components/Operation.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from './Button';
+
+interface OperationProps {
+  id: number;
+  title: string;
+  color: string;
+}
+
+const Operation: React.FC<OperationProps> = ({ id, title, color }) => {
+  const history = useHistory();
+
+  return (
+    <Button
+      backgroundColor={color}
+      value={`#${id} ${title}`}
+      onClick={() => {
+        history.push(`/operations/${id}`);
+      }}
+    />
+  );
+};
+
+export default Operation;
