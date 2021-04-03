35a859917bad8068525a80aec14e071142508623
Yihyun <hihyun16@gmail.com>
2021-04-03 21:45:30 +0900

    Refactor: rename router Home, Main to Login, Home - 명확성을 위해 기존의 Home 라우터를 Login으로 명칭 변경 - 명확성을 위해 기존의 Main 라우터를 Home으로 명칭 변경
    Issue #22

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,13 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Header from '@/components/Header';
-import Title from '@/components/Title';
+import UserInfo from '@/components/UserInfo';
+import OperationList from '@/components/OperationList';
+import Button from '@/components/Button';
+import theme from '@/style/theme';
+import Modal from '@/components/Modal';
+import JoinOperationForm from '@/components/JoinOperationForm';
+import CreateOperationForm from '@/components/CreateOperationForm';
+import { useHistory } from 'react-router-dom';
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const LeftWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  height: 80vh;
+  margin: 0 auto;
+  border-right: 1px solid ${(props) => props.theme.highlightColor};
+  text-align: center;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const RightWrapper = styled.div`
+  width: 50%;
+  height: 80vh;
+  margin: 0 auto;
+  text-align: center;
+`;
 
 const Home: React.FC = () => {
+  const history = useHistory();
+  const [visible, setVisible] = useState(false);
+  const [isJoin, setIsJoin] = useState(false);
+
+  useEffect(() => {
+    if (!localStorage.getItem('token')) history.replace('/login');
+  }, []);
+
   return (
-    <div>
+    <>
       <Header />
-      <Title />
-    </div>
+      <ContentWrapper>
+        <LeftWrapper>
+          <UserInfo />
+          <ButtonWrapper>
+            <Button
+              value="새 작전 만들기"
+              onClick={() => {
+                setIsJoin(false);
+                setVisible(!visible);
+              }}
+            />
+            <Button
+              value="작전 참여하기"
+              backgroundColor={theme.mainColor}
+              color={theme.highlightColor}
+              onClick={() => {
+                setIsJoin(true);
+                setVisible(!visible);
+              }}
+            />
+          </ButtonWrapper>
+        </LeftWrapper>
+        <RightWrapper>
+          <OperationList />
+        </RightWrapper>
+      </ContentWrapper>
+      <Modal visible={visible} setVisible={setVisible}>
+        {isJoin ? <JoinOperationForm /> : <CreateOperationForm />}
+      </Modal>
+    </>
   );
 };
 
---
+++ client/src/routes/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/components/Header';
+import Title from '@/components/Title';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Title />
+    </div>
+  );
+};
+
+export default Login;
