6970baa18006113ccfa15eba847126d1cb21f935
Yihyun <hihyun16@gmail.com>
2021-04-03 21:57:40 +0900

    Feat: implement CreateOperationForm Component - 작전 생성을 위한 CreateOperationComponent 구현
    Issue #27

--- client/src/components/CreateOperationForm.tsx
+++ client/src/components/CreateOperationForm.tsx
@@ -1,7 +1,9 @@
-import theme from '@/style/theme';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { CirclePicker } from 'react-color';
+import { ERROR } from '@/utils/message';
+import { sendPostRequest } from '@/utils/request';
+import { useHistory } from 'react-router-dom';
 import Button from './Button';
 
 const CreateOperationFormWrapper = styled.form`
@@ -36,15 +38,78 @@ const ColorPickerWrapper = styled.div`
   margin: 0.5rem auto;
 `;
 
-const CreateOperationForm: React.FC = () => {
-  const [title, setTitle] = useState('');
-  const [password, setPassword] = useState('');
+const Message = styled.p`
+  color: red;
+`;
 
+const CreateOperationForm: React.FC = () => {
   const today = new Date().toISOString().split('T')[0];
+  const history = useHistory();
+
+  const [title, setTitle] = useState('');
+  const [code, setCode] = useState('');
   const [startDate, setStartDate] = useState(today);
   const [endDate, setEndDate] = useState(today);
+  const [color, setColor] = useState('');
+  const [message, setMessage] = useState('');
 
-  const [color, setColor] = useState(theme.highlightColor);
+  const isValidTitle = () => {
+    if (title.length < 1) return false;
+    if (title[0] === ' ') return false;
+    return true;
+  };
+
+  const isValidCode = () => {
+    if (code[0] === ' ') return false;
+    return true;
+  };
+
+  const isValidDate = () => {
+    if (today > startDate || today > endDate) return false;
+    if (startDate > endDate) return false;
+    return true;
+  };
+
+  const isValidColor = () => {
+    if (!color) return false;
+    return true;
+  };
+
+  const checkData = (func: () => boolean, errorMessage: string) => {
+    const result = func();
+    if (!result) {
+      setMessage(errorMessage);
+      return false;
+    }
+    setMessage('');
+    return true;
+  };
+
+  const isValidForm = () => {
+    if (!checkData(isValidTitle, ERROR.NOT_VALID_TITLE)) return false;
+    if (!checkData(isValidCode, ERROR.NOT_VALID_CODE)) return false;
+    if (!checkData(isValidDate, ERROR.NOT_VALID_DATE)) return false;
+    if (!checkData(isValidColor, ERROR.NOT_VALID_COLOR)) return false;
+    return true;
+  };
+
+  const createOperationEvent = async () => {
+    if (!isValidForm()) return;
+    const { result, error } = await sendPostRequest('/operations', {
+      title,
+      code,
+      startDate,
+      endDate,
+      color,
+    });
+    if (error) {
+      alert('로그인 정보가 없습니다. 로그인 페이지로 이동합니다.');
+      history.push('/login');
+      return;
+    }
+    const { operationId } = result;
+    history.push(`/operations/${operationId}`);
+  };
 
   return (
     <>
@@ -55,24 +120,38 @@ const CreateOperationForm: React.FC = () => {
           value={title}
           placeholder="작전명을 입력해주세요."
           onChange={(e) => setTitle(e.target.value)}
+          onBlur={() => {
+            checkData(isValidTitle, ERROR.NOT_VALID_TITLE);
+          }}
+          maxLength={20}
         />
         <Input
           type="text"
-          value={password}
-          placeholder="암호를 설정해주세요. (필수 X)"
-          onChange={(e) => setPassword(e.target.value)}
+          value={code}
+          placeholder="(선택) 암호를 설정해주세요."
+          onChange={(e) => setCode(e.target.value)}
+          onBlur={() => {
+            checkData(isValidCode, ERROR.NOT_VALID_CODE);
+          }}
+          maxLength={8}
         />
         <h3>시작 날짜</h3>
         <Input
           type="date"
           value={startDate}
           onChange={(e) => setStartDate(e.target.value)}
+          onBlur={() => {
+            checkData(isValidDate, ERROR.NOT_VALID_DATE);
+          }}
         />
         <h3>종료 날짜</h3>
         <Input
           type="date"
           value={endDate}
           onChange={(e) => setEndDate(e.target.value)}
+          onBlur={() => {
+            checkData(isValidDate, ERROR.NOT_VALID_DATE);
+          }}
         />
         <h3>색상 선택</h3>
         <ColorPickerWrapper>
@@ -81,7 +160,14 @@ const CreateOperationForm: React.FC = () => {
             onChange={(value) => setColor(value.hex)}
           />
         </ColorPickerWrapper>
-        <Button value="생성하기" />
+        <Button
+          value="생성하기"
+          onClick={async (e) => {
+            e.preventDefault();
+            await createOperationEvent();
+          }}
+        />
+        <Message>{message}</Message>
       </CreateOperationFormWrapper>
     </>
   );
