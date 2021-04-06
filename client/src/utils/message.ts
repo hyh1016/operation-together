5db01820232049a18d076a31386797688162e243
Yihyun <hihyun16@gmail.com>
2021-04-06 16:00:20 +0900

    Feat: add error message - 작전 수정에 실패했을 시 출력되는 메시지 추가
    Issue #28

--- client/src/utils/message.ts
+++ client/src/utils/message.ts
@@ -13,6 +13,7 @@ const ERROR = {
   NOT_VALID_DATE: '날짜 설정이 잘못되었습니다.',
   NOT_VALID_COLOR: '색상을 선택해주세요.',
   OPERATION_CREATE_FAILED: '작전 생성에 실패하였습니다.',
+  OPERATION_UPDATE_FAILED: '작전 수정에 실패하였습니다.',
   NOT_VALID_OPERATION: '작전 코드 또는 암호가 잘못되었습니다.',
 };
 
