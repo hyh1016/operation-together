e6a5f2b1b672c751f40457368b98885a26a098ca
Yihyun <hihyun16@gmail.com>
2021-04-13 15:43:44 +0900

    Feat: create chart router object - chart API를 제공하기 위한 chart router 생성 - create, get API 구현
    Issue #24

---
+++ server/src/route/chart.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import passport from 'passport';
+import Container from '@/container';
+import User from '@/entity/User';
+
+const router = Router();
+
+export default (indexRouter: Router): void => {
+  indexRouter.use('/charts', router);
+  router.use('/', passport.authenticate('jwt', { session: false }));
+  router.use('/', async (req, res, next) => {
+    const { operationId } = req.body;
+    const { id: userId } = req.user as User;
+    const serviceInstance = Container.getOperationService();
+    const isValidUser = await serviceInstance.isJoinedUser(userId, operationId);
+    if (!isValidUser) return res.status(401).json();
+    return next();
+  });
+
+  router.post('/', async (req, res) => {
+    const { id: userId } = req.user as User;
+    const { operationId } = req.body;
+    const serviceInstance = Container.getChartService();
+    const result = await serviceInstance.createChart(userId, operationId);
+    if (!result) return res.status(400).json({ message: 'create failed' });
+    return res.status(200).json();
+  });
+
+  router.get('/', async (req, res) => {
+    const { operationId, checkedDate } = req.body;
+    const serviceInstance = Container.getChartService();
+    const charts = await serviceInstance.getCheckedUsers(
+      operationId,
+      checkedDate,
+    );
+    if (!charts) return res.status(400).json({ message: 'get charts failed' });
+    return res.status(200).json(Object.fromEntries(charts));
+  });
+};
