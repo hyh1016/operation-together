c515e84a3d6e826380a6069ee97a4a9976c0eb59
Yihyun <hihyun16@gmail.com>
2021-04-13 13:29:12 +0900

    Refactor: create Input common component - 공통적으로 사용되는 입력 컴포넌트 생성
    Issue #29

---
+++ client/src/components/Common/Input.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width: 80%;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  border: 3px solid #fff;
+  border-radius: 16px;
+  font-size: 1rem;
+  outline: none;
+  &:focus {
+    border: 3px solid ${(props) => props.theme.highlightColor};
+  }
+  &[type='password'] {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  &::placeholder {
+    font-family: 'BMDOHYEON';
+  }
+`;
+
+export default Input;
