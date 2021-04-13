f650524b3b4e98a2670529a4149a24a81e259088
Yihyun <hihyun16@gmail.com>
2021-04-13 15:49:07 +0900

    Feat: add chart router to index router - 새로 추가된 router를 index router에 등록
    Issue #24

--- server/src/route/index.ts
+++ server/src/route/index.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import operation from './operation';
 import user from './user';
+import operation from './operation';
+import chart from './chart';
 
 export default (): Router => {
   const router = Router();
   user(router);
   operation(router);
+  chart(router);
 
   return router;
 };
