336ec78af454d3da0a30620cae8b5fe44cf15a97
Yihyun <hihyun16@gmail.com>
2021-04-15 12:46:44 +0900

    Feat: add onClick default props to Button component - Button 컴포넌트의 onClick 메소드의 default를 지정
    Issue #28

--- client/src/components/Common/Button.tsx
+++ client/src/components/Common/Button.tsx
@@ -56,6 +56,9 @@ Button.defaultProps = {
   backgroundColor: theme.highlightColor,
   color: theme.mainColor,
   border: true,
+  onClick: (e) => {
+    e.preventDefault();
+  },
 };
 
 export default Button;
