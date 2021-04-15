e02a2f02e2f4542499759bf50da466439f06d763
Yihyun <hihyun16@gmail.com>
2021-04-15 13:00:49 +0900

    Feat: create LeaveOperationForm - 작전 탈퇴를 위한 Form Component 구현
    Issue #28

---
+++ client/src/components/Chart/LeaveOperationForm.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import { sendPutRequest } from '@/utils/request';
+import { ERROR } from '@/utils/message';
+import Form from '@/components/Common/Form';
+import Button from '@/components/Common/Button';
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+
+  Button {
+    width: 100px;
+  }
+`;
+
+interface Props {
+  operationId: number;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LeaveOperationForm: React.FC<Props> = ({ operationId, setVisible }) => {
+  const [message, setMessage] = useState('');
+  const history = useHistory();
+
+  const leaveOperationEvent = async () => {
+    const { result, error } = await sendPutRequest(
+      `/operations/${operationId}/leave`,
+    );
+    if (error) {
+      setMessage(ERROR.OPERATION_LEAVE_FAILED);
+      return;
+    }
+    history.push('/');
+  };
+
+  return (
+    <Form message={message}>
+      <h2>작전에서 탈퇴하시겠습니까?</h2>
+      <ButtonWrapper>
+        <Button
+          value="예"
+          onClick={async () => {
+            await leaveOperationEvent();
+          }}
+        />
+        <Button value="아니오" onClick={() => setVisible(false)} />
+      </ButtonWrapper>
+    </Form>
+  );
+};
+
+export default LeaveOperationForm;
