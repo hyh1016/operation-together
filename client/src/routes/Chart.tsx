a0a09fc3c0354b2ab618607e90734e1c69e2937a
Yihyun <hihyun16@gmail.com>
2021-04-20 15:26:29 +0900

    Feat: add OperationContextProvider to Chart route - Chart page에서 Operation Context를 제공하도록 함
    Issue #31

--- client/src/routes/Chart.tsx
+++ client/src/routes/Chart.tsx
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React from 'react';
 import styled from 'styled-components';
-import { Operation } from '@/interfaces';
 import Header from '@/components/Common/Header';
 import Title from '@/components/Chart/Titlte';
-import { sendGetRequest } from '@/utils/request';
+import Calendar from '@/components/Chart/Calendar';
+import UserList from '@/components/Chart/UserList';
+import { OperationContextProvider } from '@/contexts/OperationContext';
 
 const ChartWrapper = styled.div`
   width: 90%;
   margin: 0 auto;
 `;
 
-interface MatchParameter {
-  id: string;
-}
+const ContentWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  & > * {
+    padding: 1rem;
+    text-align: center;
+    /* color: ${(props) => props.theme.mainColor}; */
+  }
+`;
 
 const Chart: React.FC = () => {
-  const match = useRouteMatch<MatchParameter>();
-  const { id } = match.params;
-
-  const [operation, setOperation] = useState<Operation | undefined>();
-
-  useEffect(() => {
-    const fetch = async () => {
-      const { result, error } = await sendGetRequest(`/operations/${id}`);
-      if (error) return;
-      setOperation(result.operation);
-    };
-    fetch();
-  }, []);
-
   return (
     <>
-      <Header />
-      <ChartWrapper>
-        {operation ? (
-          <Title operation={operation} setOperation={setOperation} />
-        ) : undefined}
-      </ChartWrapper>
+      <OperationContextProvider>
+        <Header />
+        <ChartWrapper>
+          <Title />
+          <ContentWrapper>
+            <Calendar />
+            <UserList />
+          </ContentWrapper>
+        </ChartWrapper>
+      </OperationContextProvider>
     </>
   );
 };
