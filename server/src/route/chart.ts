2b7a5282729d59fe28ef1d7470e5df07e3077c47
Yihyun <hihyun16@gmail.com>
2021-04-21 00:12:30 +0900

    Fix: solve get chart error - 차트를 반환하는 메소드의 형식 변경 및 에러 픽스
    Issue #24

--- server/src/route/chart.ts
+++ server/src/route/chart.ts
@@ -2,35 +2,46 @@ import { Router } from 'express';
 import passport from 'passport';
 import Container from '@/container';
 import User from '@/entity/User';
+import { isNumber } from '@/util/util';
 
 const router = Router();
 
 export default (indexRouter: Router): void => {
   indexRouter.use('/charts', router);
   router.use('/', passport.authenticate('jwt', { session: false }));
-  router.use('/', async (req, res, next) => {
-    const { operationId } = req.body;
-    const { id: userId } = req.user as User;
-    const serviceInstance = Container.getOperationService();
-    const isValidUser = await serviceInstance.isJoinedUser(userId, operationId);
-    if (!isValidUser) return res.status(401).json();
-    return next();
-  });
 
-  router.post('/', async (req, res) => {
+  router.post('/', async (req, res, next) => {
     const { id: userId } = req.user as User;
     const { operationId } = req.body;
-    const serviceInstance = Container.getChartService();
-    const result = await serviceInstance.createChart(userId, operationId);
+    if (!isNumber(operationId)) return next();
+    const operationInstance = Container.getOperationService();
+    const isValidUser = await operationInstance.isJoinedUser(
+      userId,
+      operationId,
+    );
+    if (!isValidUser) return res.status(401).json();
+
+    const chartInstance = Container.getChartService();
+    const result = await chartInstance.createChart(userId, operationId);
     if (!result) return res.status(400).json({ message: 'create failed' });
+
     return res.status(200).json();
   });
 
-  router.get('/', async (req, res) => {
-    const { operationId, checkedDate } = req.body;
-    const serviceInstance = Container.getChartService();
-    const users = await serviceInstance.getCheckedUsers(
-      operationId,
+  router.get('/:id/:date', async (req, res, next) => {
+    const { id: operationId, date: checkedDate } = req.params;
+    const { id: userId } = req.user as User;
+    if (!isNumber(operationId)) return next();
+    const operationInstance = Container.getOperationService();
+    const isValidUser = await operationInstance.isJoinedUser(
+      userId,
+      Number(operationId),
+    );
+    if (!isValidUser) return res.status(401).json();
+
+    const chartInstance = Container.getChartService();
+    const users = await chartInstance.getCheckedUsers(
+      Number(operationId),
       checkedDate,
     );
     if (!users) return res.status(400).json({ message: 'get failed' });
