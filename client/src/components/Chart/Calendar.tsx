fc3d1f4717ed7b9aefd5b1fd1462f6e0dda1a499
Yihyun <hihyun16@gmail.com>
2021-04-20 21:58:39 +0900

    Feat: Calendar days mapping Button - 특정 일자를 누를 시 세부 일자를 렌더링하기 위하여 각 일자를 Button Component로 구성
    Issue #28

--- client/src/components/Chart/Calendar.tsx
+++ client/src/components/Chart/Calendar.tsx
@@ -2,19 +2,38 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import dayjs from 'dayjs';
 import { useOperation } from '@/contexts/OperationContext';
+import Button from '@/components/Common/Button';
+import getKoDay from '@/utils/getKoDay';
+import getToday from '@/utils/getToday';
 
-const CalendarWrapper = styled.div`
+const CalendarWrapper = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
   width: 80%;
   background-color: rgba(255, 255, 255, 0.6);
   color: ${(props) => props.theme.mainColor};
 `;
+
+const DayList = styled.li`
+  width: 14%;
+  list-style-type: none;
+
+  Button {
+    width: 100%;
+    font-size: 1rem;
+  }
+`;
+
+interface Props {
+  setSelectedDay: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
 
 interface Day {
   today: string;
   startFrom: number;
 }
 
-const Calendar: React.FC = () => {
+const Calendar: React.FC<Props> = ({ setSelectedDay }) => {
   const operation = useOperation();
   const [days, setDays] = useState<Day[] | undefined>();
 
@@ -27,7 +46,7 @@ const Calendar: React.FC = () => {
     const allDay = Array.from(Array(diff + 1).keys());
     setDays(
       allDay.map((v) => ({
-        today: start.add(v, 'day').format('YYYY-MM-DD'),
+        today: start.add(v, 'day').locale('ko').format('YYYY-MM-DD'),
         startFrom: v + 1,
       })),
     );
@@ -37,9 +56,18 @@ const Calendar: React.FC = () => {
     <>
       <CalendarWrapper>
         {days?.map((v) => (
-          <div key={v.startFrom}>
-            {v.today} Day {v.startFrom}
-          </div>
+          <DayList key={v.startFrom}>
+            <Button
+              backgroundColor={v.today === getToday() ? undefined : '#ccc'}
+              value={
+                <>
+                  <p>{`${v.today} (${getKoDay(v.today)})`}</p>
+                  <p>{`D-${v.startFrom}`}</p>
+                </>
+              }
+              onClick={() => setSelectedDay(v.today)}
+            />
+          </DayList>
         ))}
       </CalendarWrapper>
     </>
