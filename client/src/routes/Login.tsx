15634fc41d14ee5d41d027afa971bfbe47560f67
Yihyun <hihyun16@gmail.com>
2021-04-26 14:50:43 +0900

    Feat: login page check token - 기본 페이지는 login page이나 token이 존재할 시 main으로 redirect
    Issue #32

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '@/components/Common/Header';
 import Title from '@/components/Login/Title';
 
 const Login: React.FC = () => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) history.replace('/main');
+  }, []);
+
   return (
     <div>
       <Header />
