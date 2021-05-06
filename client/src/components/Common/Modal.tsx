ba06c4aca4de22f43ab043014e7dfd3e0336c9be
Yihyun <hihyun16@gmail.com>
2021-05-06 15:03:50 +0900

    Fix: add !important to modal style - modal overlay가 parent component의 style 영향을 받아 발생하는 오류 해결을 위해 !important 성질 추가
    Issue #41

--- client/src/components/Common/Modal.tsx
+++ client/src/components/Common/Modal.tsx
@@ -4,12 +4,17 @@ import styled from 'styled-components';
 import Button from './Button';
 
 const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.5);
+  &&& {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    max-width: 100vw;
+    height: 100vh;
+    max-height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+    overflow: hidden;
+  }
 `;
 
 const ModalWrapper = styled.div`
