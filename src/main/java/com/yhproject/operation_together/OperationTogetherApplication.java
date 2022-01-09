ea4f32fb6580f110646505372b593ba1c648f260
Yihyun <hihyun16@gmail.com>
2022-01-09 17:52:43 +0900

    Refactor: Initialize Spring Boot Project - Spring Boot 프로젝트 초기화
    Issue #44

---
+++ src/main/java/com/yhproject/operation_together/OperationTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.yhproject.operation_together;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhproject/operation_together/OperationTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhproject.operation_together;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
