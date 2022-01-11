7856250a40ac482e0f4bb9565f7d45ae769a95cb
Yihyun <hihyun16@gmail.com>
2022-01-11 16:32:40 +0900

    Refactor: Add Builder pattern to Operaiton Entity - 객체 생성 시 가독성 증진 및 실수 방지를 위해 Builder Pattern 추가
    Issue #47

--- src/main/java/com/yhproject/operation_together/domain/operation/Operation.java
+++ src/main/java/com/yhproject/operation_together/domain/operation/Operation.java
@@ -2,13 +2,16 @@ package com.yhproject.operation_together.domain.operation;
 
 import com.yhproject.operation_together.domain.BaseTimeEntity;
 import com.yhproject.operation_together.domain.input.Input;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 @Entity
 public class Operation extends BaseTimeEntity {
 
@@ -28,4 +31,13 @@ public class Operation extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "operation")
     public List<Input> inputs;
+
+    @Builder
+    public Operation(String name, String password, String link, LocalDateTime operationDay, int type) {
+        this.name = name;
+        this.password = password;
+        this.link = link;
+        this.operationDay = operationDay;
+        this.type = type;
+    }
 }
