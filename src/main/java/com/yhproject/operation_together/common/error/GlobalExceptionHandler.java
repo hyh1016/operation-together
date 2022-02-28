2a03fb7565effdc07b2a2228777c5abdbb15dba3
Yihyun <hihyun16@gmail.com>
2022-02-28 16:10:39 +0900

    Feat: Add GlobalExceptionHandler - 전역적으로 예외를 관리할 Handler 선언
    Issue #69

---
+++ src/main/java/com/yhproject/operation_together/common/error/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.yhproject.operation_together.common.error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
