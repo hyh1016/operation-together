c0e14cd0369ab2d299d0958d79716cb6af91ee91
YIHYUN HA <hihyun16@gmail.com>
2022-11-18 22:01:23 +0900

    Fix: Add getter to ErrorResponse for create request body - Getter가 없어 body로 변환되지 못하는 문제 해결
    Issue #88

--- src/main/java/com/yhproject/operation_together/common/exception/ErrorResponse.java
+++ src/main/java/com/yhproject/operation_together/common/exception/ErrorResponse.java
@@ -1,9 +1,11 @@
 package com.yhproject.operation_together.common.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
+@Getter
 public class ErrorResponse {
 
     private final HttpStatus status;
