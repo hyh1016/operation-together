d2ad0931917cc585b81513a3ae3391b0cc8ac6b0
YIHYUN HA <hihyun16@gmail.com>
2022-12-01 20:59:18 +0900

    Feat: add InternalServerException class - 5xx 응답으로 처리할 예외 클래스 선언 - 추후 세부화 여부 검토
    Issue #90

---
+++ src/main/java/com/yhproject/operation_together/common/exception/InternalServerException.java
@@ -0,0 +1,9 @@
+package com.yhproject.operation_together.common.exception;
+
+public class InternalServerException extends RuntimeException {
+
+    public InternalServerException(String message) {
+        super(message);
+    }
+
+}
