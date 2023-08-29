fe4878dd6a37e8d05236faaeba95e35ecbb52284
YIHYUN HA <hihyun16@gmail.com>
2023-08-29 23:15:16 +0900

    Test: 기존 테스트 코드를 H2 DB 환경에서 성공하도록 변경
    Issue #97

--- src/test/java/com/yhproject/operation_together/web/OperationControllerTest.java
+++ src/test/java/com/yhproject/operation_together/web/OperationControllerTest.java
@@ -6,6 +6,7 @@ import com.yhproject.operation_together.operation.dto.CreateOperationRequest;
 import com.yhproject.operation_together.operation.entity.Operation;
 import com.yhproject.operation_together.operation.entity.OperationRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,12 +18,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OperationControllerTest {
 
 	@LocalServerPort
@@ -35,12 +37,13 @@ public class OperationControllerTest {
 	private MockMvc mvc;
 
 	@AfterEach
-	public void tearDown() throws Exception {
+	public void tearDown() {
 		operationRepository.deleteAll();
 	}
 
+	@DisplayName("작전 생성 - 성공")
 	@Test
-	public void createOperationTest() throws Exception {
+	public void create_success() throws Exception {
 		// given
 		String name = "name";
 		String password = "password";
@@ -51,7 +54,7 @@ public class OperationControllerTest {
 				.operationDate(operationDate)
 				.build();
 
-		String url = "http://localhost:%s/api/operations".formatted(port);
+		String url = "http://localhost:" + port + "/api/operations";
 
 		// when
 		mvc.perform(post(url)
@@ -60,8 +63,15 @@ public class OperationControllerTest {
 				.andExpect(status().isOk());
 
 		// then
-		List<Operation> all = operationRepository.findAll();
-		assertThat(all.get(0).getLink().length()).isEqualTo(36);
+		List<Operation> operationList = operationRepository.findAll();
+		assertEquals(1, operationList.size());
+
+		Operation operation = operationList.get(0);
+		assertTrue(operation.getId() > 0);
+		assertEquals(36, operation.getLink().length());
+		assertEquals(dto.getName(), operation.getName());
+		assertEquals(dto.getPassword(), operation.getPassword());
+		assertEquals(dto.getOperationDate(), operation.getOperationDate());
 	}
 
 }
