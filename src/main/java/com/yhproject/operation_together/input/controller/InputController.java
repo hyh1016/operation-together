6f46baa207b1bf3b92c2041cf293fceab8b1af91
YIHYUN HA <hihyun16@gmail.com>
2023-09-03 17:48:19 +0900

    Refact: 일부 API를 용도에 맞게 Operation 컨트롤러에서 Input 컨트롤러로 이동
    작전 입력 목록의 조회, 입력을 기반으로 최종 작전을 생성하는 것은 작전의 입력을 다루는 Input의 영역
    Issue #99

--- src/main/java/com/yhproject/operation_together/input/controller/InputController.java
+++ src/main/java/com/yhproject/operation_together/input/controller/InputController.java
@@ -1,7 +1,10 @@
 package com.yhproject.operation_together.input.controller;
 
+import com.yhproject.operation_together.common.config.CheckAuth;
 import com.yhproject.operation_together.common.dto.EmptyJSON;
 import com.yhproject.operation_together.input.dto.CreateInputRequest;
+import com.yhproject.operation_together.input.dto.InputDto;
+import com.yhproject.operation_together.input.dto.ResultResponse;
 import com.yhproject.operation_together.input.service.InputService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -10,18 +13,32 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
-@RequestMapping("/api/inputs")
+@RequestMapping("/api/operations/{link}/inputs")
 @RestController
 @RequiredArgsConstructor
 public class InputController {
 
     private final InputService inputService;
 
-    @PostMapping(value = "/{link}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EmptyJSON> createInput(@PathVariable String link, @RequestBody @Valid CreateInputRequest dto) {
         log.info("{} 작전에 새 입력 추가 {}", link, dto);
         return ResponseEntity.ok(inputService.createInput(link, dto));
+    }
+
+    @CheckAuth
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<InputDto>> getInputs(@PathVariable String link) {
+        return ResponseEntity.ok(inputService.getInputList(link));
+    }
+
+    @CheckAuth
+    @GetMapping(value = "/result", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ResultResponse>> getResult(@PathVariable String link) {
+        return ResponseEntity.ok(inputService.getResultList(link));
     }
 
 }
--- src/main/java/com/yhproject/operation_together/operation/controller/OperationController.java
+++ src/main/java/com/yhproject/operation_together/operation/controller/OperationController.java
@@ -1,10 +1,6 @@
 package com.yhproject.operation_together.operation.controller;
 
-import com.yhproject.operation_together.common.config.CheckAuth;
 import com.yhproject.operation_together.common.dto.EmptyJSON;
-import com.yhproject.operation_together.input.dto.InputDto;
-import com.yhproject.operation_together.input.dto.ResultResponse;
-import com.yhproject.operation_together.input.service.InputService;
 import com.yhproject.operation_together.operation.dto.CheckPasswordRequest;
 import com.yhproject.operation_together.operation.dto.CreateOperationRequest;
 import com.yhproject.operation_together.operation.dto.CreateOperationResponse;
@@ -17,8 +13,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @Slf4j
 @RequestMapping("/api/operations")
 @RestController
@@ -26,7 +20,6 @@ import java.util.List;
 public class OperationController {
 
     private final OperationService operationService;
-    private final InputService inputService;
 
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CreateOperationResponse> createOperation(@RequestBody @Valid CreateOperationRequest requestDto) {
@@ -44,18 +37,6 @@ public class OperationController {
     @PostMapping(value = "/{link}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EmptyJSON> checkPassword(@PathVariable String link, @RequestBody CheckPasswordRequest dto) throws Exception {
         return ResponseEntity.ok(operationService.checkPassword(link, dto));
-    }
-
-    @CheckAuth
-    @GetMapping(value = "/{link}/inputs", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<InputDto>> getInputs(@PathVariable String link) {
-        return ResponseEntity.ok(inputService.getInputList(link));
-    }
-
-    @CheckAuth
-    @GetMapping(value = "/{link}/result", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<ResultResponse>> getResponse(@PathVariable String link) {
-        return ResponseEntity.ok(inputService.getResultList(link));
     }
 
 }
