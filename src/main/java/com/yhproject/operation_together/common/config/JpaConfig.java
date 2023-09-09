dc8d3fdc9682c7bf7c8fc4f01ed9b16acb1eb757
YIHYUN HA <hihyun16@gmail.com>
2023-09-09 17:27:37 +0900

    Fix: 데이터의 생성/수정일자가 기록되지 않는 문제 해결
    누락된 어노테이션 추가
    Issue #97

--- src/main/java/com/yhproject/operation_together/common/config/JpaConfig.java
+++ src/main/java/com/yhproject/operation_together/common/config/JpaConfig.java
@@ -1,8 +1,10 @@
 package com.yhproject.operation_together.common.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@Configuration
 public class JpaConfig {
 
 }
